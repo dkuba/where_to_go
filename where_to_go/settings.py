56c97532093d57997b7c9589d8dafab8a0e56c27
Dmitriy.Kubarev <dmitriy.kubarev@russdragmet.ru>
2021-02-11 15:27:14 +0300

    Create places app

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [ BASE_DIR, "static", ]
+STATICFILES_DIRS = [BASE_DIR, "static", ]
 
