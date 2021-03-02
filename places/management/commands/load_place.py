81dc243c4c0c08d8222a5dec0231b7a1c0034271
Dmitriy.Kubarev <dmitriy.kubarev@russdragmet.ru>
2021-03-02 10:21:56 +0300

    catch http requests errors

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import requests
@@ -12,14 +13,27 @@ class Command(BaseCommand):
         parser.add_argument('place_json_url', type=str)
 
     def handle(self, *args, **options):
-        place = requests.get(url=options['place_json_url']).json()
-        place = Place.objects.get_or_create(
-            title=place['title'],
-            defaults={'description_short': place['description_short'],
-                      'description_long': place['description_long'],
-                      'latitude': place['coordinates']['lat'],
-                      'longitude': place['coordinates']['lng']}
+
+        try:
+            response = requests.get(url=options['place_json_url'])
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            logging.error(err)
+            return
+
+        decoded_response = response.json()
+        if 'error' in decoded_response:
+            logging.error('Сервер вернул ошибку')
+            return
+
+        place_entity = Place.objects.get_or_create(
+            title=decoded_response['title'],
+            defaults={'short_description': decoded_response['description_short'],
+                      'long_description': decoded_response['description_long'],
+                      'latitude': decoded_response['coordinates']['lat'],
+                      'longitude': decoded_response['coordinates']['lng']}
         )
-        for image_url in place['imgs']:
+
+        for image_url in decoded_response['imgs']:
             new_url = os.path.join('places', image_url.split('media')[1].replace('/', ''))
-            Image.objects.create(place=place, image=new_url)
+            Image.objects.create(place=place_entity[0], image=new_url)
